4d810d2f8d4e5638f9e17ff9a93de75ec9b78088
santosbio2 <santosbio@me.com>
2014-03-20 15:14:52 -0300

    Variáveis e README
    - As variáveis de conexão agora são setadas fora da classe, facilitando
    a reutilização do código
    - Inclusão do arquivo README.md, com as informações sobre a utilização
    da classe.

--- Conexao.class.php
+++ Conexao.class.php
@@ -2,6 +2,10 @@
 /**
 *  Classe para conexão ao banco de dados, setando a codificação dos caracteres como UTF-8
 *
+*  Utilização:
+*  $conexao = new Conexao();
+*  $conexao->conecta('host', 'user', 'password', 'db_name');
+*
 *  Autor: Adriano dos Santos
 *  E-mail: santosbio@gmail.com
 *
@@ -9,21 +13,12 @@
 
 class Conexao
 {
-   private $_host = ''; // Host do deu DB. Provavelmente, localhost
-   private $_user = ''; // User do seu DB
-   private $_pass = ''; // Senha do seu DB
-   private $_database = ''; // Nome do seu DB
    public  $_con;
- 
-   function __construct()
+
+   function conecta($_host, $_user, $_pass, $_database)
    {
-       $this->conecta();
-   }
- 
-   function conecta()
-   {
-        $_con = mysql_connect($this->_host, $this->_user, $this->_pass) or die("Erro ao conectar ao servidor &raquo; " . mysql_error());
-        $_con = mysql_select_db($this->_database) or die("Erro ao selecionar o Banco de Dados &raquo; " . mysql_error());
+        $_con = mysql_connect($_host, $_user, $_pass) or die("Erro ao conectar ao servidor &raquo; " . mysql_error());
+        $_con = mysql_select_db($_database) or die("Erro ao selecionar o Banco de Dados &raquo; " . mysql_error());
         mysql_query("SET NAMES 'utf8'");
         mysql_query('SET character_set_connection=utf8');
         mysql_query('SET character_set_client=utf8');
---
+++ README.md
@@ -0,0 +1,10 @@
+#CONEXÃO COM BANCO DE DADOS COM CODIFICAÇÃO UTF-8
+
+Classe para conexão ao banco de dados, setando a codificação dos caracteres como UTF-8
+
+
+### Utilização
+```php
+  $conexao = new Conexao();
+  $conexao->conecta('host', 'user', 'password', 'db_name');
+```
